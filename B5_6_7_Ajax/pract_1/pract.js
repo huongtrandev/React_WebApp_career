c1697d70e32d90a125de8dae2c923ec7d9889268
huongtrandev <huongtq.dev@gmail.com>
2022-06-17 06:59:01 +0700

    thuc hanh ajax

---
+++ B5_6_7_Ajax/pract_1/pract.js
@@ -0,0 +1,24 @@
+// function greet(name, callback) {
+//   console.log("hi" + " " + name);
+//   callback();
+// }
+// // callback function la ham goi sau, trong do callback duoc coi la 1 tham so trong ham
+// //call back function
+// function callMe() {
+//   console.log("I am callback function");
+// }
+
+// // passing function as an argument
+// greet("Peter", callMe);
+
+function greet() {
+  console.log("hello world");
+}
+
+function sayName(name) {
+  console.log("hello" + " " + name);
+}
+
+//calling function
+setTimeout(greet, 2000);
+sayName("John");
