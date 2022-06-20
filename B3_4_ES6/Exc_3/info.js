d68ed2b677ef6b7f4acf0c5837d1901ffdc9acb5
huongtrandev <huongtq.dev@gmail.com>
2022-06-20 23:44:23 +0700

    thuc hanh 20.6.2022

---
+++ B3_4_ES6/Exc_3/info.js
@@ -0,0 +1,9 @@
+class Information {
+  constructor(name, description) {
+    this.name = name;
+    this.description = description;
+  }
+  static multi(a = 1) {
+    return a * 3;
+  }
+}
--- B8_react/my-aoo/src/index.js
+++ B8_react/my-aoo/src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+// import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function formatName(user) {
+  return user.firstName + " " + user.lastName;
+}
+const user = {
+  firstName: " Harper",
+  lastName: "Perez",
+};
+const element = <h1>Hello, {formatName(user)}</h1>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  element
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
