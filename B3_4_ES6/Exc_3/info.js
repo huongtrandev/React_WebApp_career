61be90813130ad401660425ef1acc11d9c52a707
huongtrandev <huongtq.dev@gmail.com>
2022-06-21 21:18:39 +0700

    es6 bai tap 2

--- B3_4_ES6/Exc_3/info.js
+++ B3_4_ES6/Exc_3/info.js
@@ -7,3 +7,20 @@ class Information {
     return a * 3;
   }
 }
+
+class Geometry extends Information {
+  // khi extend một class thì phải dùng từ khóa super để gọi lớp cha
+  //trước khi khởi tạo thuộc tính của lớp con
+  constructor(name, description, length) {
+    super(name, description);
+    this.length = length;
+  }
+  static plus(b) {
+    return b + b;
+  }
+}
+
+let lineToHigh = new Geometry("line", "redline", 20);
+console.log(lineToHigh.description);
+Geometry.plus(50);
+console.log(Geometry.plus(50));
