e9d322012fbbb67690d7c0565eb5d0e332c4c51f
huongtrandev <huongtq.dev@gmail.com>
2022-06-23 01:41:57 +0700

    ES6 ajax

--- B5_6_7_Ajax/demo_2/demo.js
+++ B5_6_7_Ajax/demo_2/demo.js
@@ -1,9 +1,47 @@
 // async function example
 
-async function f() {
-  console.log("Async function.");
-  return Promise.resolve(1);
-}
-f().then(function (result) {
-  console.log(result);
+// async function f() {
+//   console.log("Async function.");
+//   return Promise.resolve(1);
+// }
+// f().then(function (result) {
+//   console.log(result);
+// });
+
+// //a promise
+// let promise = new Promise(function (resolve, reject) {
+//   setTimeout(function () {
+//     resolve("Promise resolved");
+//   }, 4000);
+// });
+
+// //async function
+// async function asyncFunc() {
+//   // wait until the promise resolves
+//   let result = await promise;
+//   console.log(result);
+//   console.log("hello");
+// }
+
+// //calling the async function
+// asyncFunc();
+// a promise
+let promise = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    resolve("Promise resolved");
+  }, 4000);
 });
+
+// async function
+async function asyncFunc() {
+  try {
+    // wait until the promise resolves
+    let result = await promise;
+
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// calling the async function
