f54313f0616b6073da54c754c293b810a1f65925
huongtrandev <huongtq.dev@gmail.com>
2022-07-22 23:48:43 +0700

    22072022

--- B10_ReactComponent/pract-1/src/App.js
+++ B10_ReactComponent/pract-1/src/App.js
@@ -1,8 +1,9 @@
 // import logo from "./logo.svg";
 import "./App.css";
 // import AddComponent from "./components/AddComponent";
-import Alert from "./components/Alert";
+// import Alert from "./components/Alert";
 // import Welcome from "./components/Welcomes";
+import StudentInfo from "./components/StudentInfo";
 
 function App() {
   //   return (
@@ -12,6 +13,7 @@ function App() {
   //   );
   // }
   // return <AddComponent firstNumber={1} secondNumber={2} />;
-  return <Alert text="Cảnh báo! Tài nguyên bạn vừa truy cập không tồn tại" />;
+  // return <Alert text="Cảnh báo! Tài nguyên bạn vừa truy cập không tồn tại" />;
+  return <StudentInfo />;
 }
 export default App;
--- B10_ReactComponent/pract-1/src/components/StudentInfo.js
+++ B10_ReactComponent/pract-1/src/components/StudentInfo.js
@@ -1,18 +1,58 @@
-import { Component } from "react";
+// import { Component } from "react";
 
-class StudentInfo extends Component {
-  render() {
-    return student.map((student) => (
-      <tr>
-        <td>{this.props.student.id}</td>
-        <td>{this.props.student.name}</td>
-        <td>{this.props.student.age}</td>
-        <td>{this.props.student.address}</td>
-      </tr>
-    ));
-  }
+const student = [
+  {
+    id: 0,
+    name: "nguyễn văn A",
+    age: 30,
+    adress: "hà nội",
+  },
+  {
+    id: 1,
+    name: "nguyễn văn B",
+    age: 20,
+    adress: "hà nam",
+  },
+  {
+    id: 2,
+    name: "nguyễn văn nam",
+    age: 23,
+    adress: "quảng nam",
+  },
+];
+
+function StudentInfo() {
+  return (
+    <div>
+      <table
+        style={{
+          borderWidth: "1px",
+          borderColor: "#aaaaaa",
+          borderStyle: "solid",
+        }}
+      >
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>name</th>
+            <th>age</th>
+            <th>adress</th>
+          </tr>
+        </thead>
+        {student.map((res) => (
+          <tbody key={res.id}>
+            <tr>
+              <td>{res.id}</td>
+              <td>{res.name}</td>
+              <td>{res.age}</td>
+              <td>{res.adress}</td>
+            </tr>
+          </tbody>
+        ))}
+      </table>
+    </div>
+  );
 }
-
 export default StudentInfo;
 
 // dữ liệu để trong component hay để ngoài
