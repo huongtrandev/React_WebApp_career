7acb2c45fc2a5f2d209fd9fb7535d702fbbf1687
huongtrandev <huongtq.dev@gmail.com>
2022-07-31 22:55:06 +0700

    thuc hanh state

---
+++ B11_ReactState/demo/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       header: "Header from state...",
+//       content: "Content from state...",
+//     };
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>{this.state.header}</h1>
+//         <h2>{this.state.content}</h2>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+//state là nơi lưu trữ giá trị thuộc tính thuộc về component
+//state thay đổi thì component render lại
+//state là một object chứa dữ liệu hoặc thông tin về component
